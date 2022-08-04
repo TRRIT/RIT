b833f56a34196cb5db843e192c25d4e1c2ec4869
TRRIT <sasha-timofeeva-1994@mail.ru>
2022-08-05 02:54:37 +0300

    поправил что указали в каментах

--- src/test/java/quest1/MyListTest.java
+++ src/test/java/quest1/MyListTest.java
@@ -7,66 +7,71 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MyListTest {
 
+
     @Test
-    void add() {
-       MyList<Integer> test = new MyList<>();
-       int sizeOld = test.size();
-       test.add(1);
-       int sizeNew = test.size();
+    void addMustIncreaseSize1() {
+        MyList<Integer> test = new MyList<>();
+        int sizeOld = test.size();
+        test.add(1);
+        int sizeNew = test.size();
 
-
-        Assertions.assertEquals(sizeOld + 1 , sizeNew, " 1 должно быть равно 1");
+        Assertions.assertEquals(sizeOld + 1, sizeNew, " 1 должно быть равно 1");
     }
 
     @Test
-    void testAdd() {
+    void addMustIncreaseSize2() {
         MyList<Integer> test = new MyList<>();
         int sizeOld = test.size();
         test.add(null);
         test.add(1);
         int sizeNew = test.size();
 
-        Assertions.assertEquals(sizeOld + 2 , sizeNew, "не может добавить null");
-        Assertions.assertEquals(test.data[1],1,"не добавил элемент");
+        Assertions.assertEquals(sizeOld + 2, sizeNew, "не может добавить null");
+        Assertions.assertEquals(test.data[1], 1, "не добавил элемент");
     }
 
     @Test
-    void test2Add(){
+    void addMustIncreaseSize3() {
         MyList<Integer> test = new MyList<>();
         test.add(null);
         test.add(null);
         int sizeOld = test.size();
-        test.add(0,1);
+        test.add(0, 1);
         int sizeNew = test.size();
         Assertions.assertEquals(sizeOld + 1, sizeNew, "не сдвинул элементы массива");
-        Assertions.assertEquals(test.data[0],1,"не добавил элемент");
+        Assertions.assertEquals(test.data[0], 1, "не добавил элемент");
         assertNull(test.data[2], "не добавил элемент");
     }
 
     @Test
-    void replace() {
+    void shouldReplaceTheElement() {
         MyList<Integer> test = new MyList<>();
         test.add(null);
-        test.replace(1,0);
-        Assertions.assertEquals(test.data[0],1,"не заменил значение");
+        test.replace(1, 0);
+        Assertions.assertEquals(test.data[0], 1, "не заменил значение");
     }
 
     @Test
-    void remove() {
-        MyList<String> test = new MyList<>();
+    void mustDeleteByElement1() {
+        MyList<Object> test = new MyList<>();
         test.add("0");
         test.add("1");
         test.add("0");
         int oldSize = test.size();
         test.remove("0");
         int newSize = test.size();
-        Assertions.assertEquals("1",test.data[0],"не удалил элемент ");
-        Assertions.assertEquals("0",test.data[1], "удалил лишний элемент ");
-        Assertions.assertEquals(oldSize - 1,newSize, "не сдвинул массив после удаления ");
+        MyList<String> test2 = new MyList<>();
+        test2.add("1");
+        test2.add("0");
+
+        Assertions.assertEquals("1", test.data[0], "элементы после удаления должны сдвинуться влево, замещая удаленный эелемент ");
+        Assertions.assertEquals("0", test.data[1], "удалил лишний элемент ");
+        Assertions.assertEquals(oldSize - 1, newSize, "размер массива не уменьшился");
+        Assertions.assertArrayEquals(test2.data, test.data, "не верные элементы в массиве");
     }
 
     @Test
-    void testRemove() {
+    void mustDeleteByIndex() {
         MyList<String> test = new MyList<>();
         test.add("0");
         test.add("1");
@@ -74,37 +79,38 @@ class MyListTest {
         int oldSize = test.size();
         test.remove(0);
         int newSize = test.size();
-        Assertions.assertEquals("1",test.data[0],"не удалил элемент ");
-        Assertions.assertEquals(oldSize - 1,newSize, "не сдвинул массив после удаления");
+        Assertions.assertEquals("1", test.data[0], "не удалил элемент ");
+        Assertions.assertEquals(oldSize - 1, newSize, "не сдвинул массив после удаления");
     }
 
     @Test
-    void size() {
+    void checkSize() {
         MyList<String> test = new MyList<>();
-        Assertions.assertEquals(0,test.size(),"не соответствует размеру");
+        Assertions.assertEquals(0, test.size(), "размер листа должен равен 0");
 
         test.add("0");
         test.add("1");
         test.add("0");
-        Assertions.assertEquals(3,test.size(),"не соответствует размеру");
+        Assertions.assertEquals(3, test.size(), "размер листа должен равен 3");
     }
 
     @Test
-    void reduce() {
+    void deleteNull() {
         MyList<String> test = new MyList<>();
         test.add(null);
         test.add(null);
         test.add(null);
+      //  test.add("1");
         int oldSize = test.size();
         test.reduce();
-        Assertions.assertEquals(oldSize - 3, test.size(),"не удалил null");
+        Assertions.assertEquals(oldSize - 3, test.size(), "не удалил null");
     }
 
     @Test
-    void todata() {
-        var listTest = new MyList<String>();
-        listTest.add("HO HO HO");
-        String[] strings = listTest.toArray(new String[0]);
-        String[] strings1 = listTest.toArray(new String[10]);
+    void ReturnsNewArrayOfTheElementOfThisList() {
+        String[] strings = new String[3];
+        MyList<String> test = new MyList<>();
+        strings = test.toArray(strings);
+        Assertions.assertArrayEquals(test.data, strings,"массивы отличаются");
     }
 }
