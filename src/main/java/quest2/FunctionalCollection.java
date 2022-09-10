7f6c1bb3171f8819970d2872ed99f7ffc0bde0a0
Boeing1337 <56965556+Boeing1337@users.noreply.github.com>
2022-09-10 13:07:25 +0300

    Rit branch (#13)
    * +  changed List interface
    
    * added stage 2
    
    Co-authored-by: Boeing <pm.to.dmitry@gmail.com>

--- src/main/java/quest2/FunctionalCollection.java
+++ src/main/java/quest2/FunctionalCollection.java
@@ -2,10 +2,44 @@ package quest2;
 
 import quest1.List;
 
+import java.util.Comparator;
+import java.util.function.Function;
+
 public interface FunctionalCollection<E> extends List<E> {
 
-    //create your modifying all elements method here.
+    /**
+     * creates new collection of the same type class and fill it with result of {@param function}
+     * applying the function for each element and doesn't change order
+     * <p>
+     * Example
+     * if we have such element ["A", "B", "C"] and function like ( e -> (int) e.charAt(0) )
+     * method must produce a new collection [10, 11, 12]
+     * <p>
+     * 1) Order of elements should be the same
+     * 2) Method must not crash if list contains null elements
+     * 3)
+     *
+     * @param function function that must be applied
+     * @param <T>      type of resulting collection
+     * @return resulting collection
+     */
 
-    //create your sort method here.
+    <T> FunctionalCollection<T> modify(Function<T, E> function);
+
+    /**
+     * Sort this list by ascending order. Null values must be considered as biggest values
+     *
+     * @return self (this)
+     */
+    FunctionalCollection<E> sort();
+
+
+    /**
+     * Sort this list by specified function.
+     *
+     * @return self (this)
+     */
+    FunctionalCollection<E> sortBy(Comparator<E> comparator);
+
 
 }
--- src/main/java/quest2/README.md
+++ src/main/java/quest2/README.md
@@ -1,10 +1,10 @@
-Task 1 
-#Exchange You List By additional Interface
+Task 1
+
+# Exchange You List By additional Interface
 
 You should
+
 1) implement FunctionalCollection interface by your existing custom list class.
-2) find out and create a new method which can be used for modifying all elements in your list (the more functional it is the better)
-3) create a new method which can be using for sorting the list with custom comparison 
-4) implement all new not implemented yet methods
-5) generate tests by IDEA for each new method
-6) write tests
+2) implement all new not implemented yet methods
+3) generate tests by IDEA for each new method
+4) write tests
